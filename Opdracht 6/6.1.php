a2554dac95dc1220a64390b9ffefdcba66bec904
Sofie <sofiebrink@yahoo.com>
2022-11-30 11:49:13 +0100

    Opdracht 6.1

---
+++ Opdracht 6/6.1.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello</title>
+</head>
+<body>
+<?php
+    if(isset($_SESSION["Counter"])) {
+        $_SESSION["Counter"]++;
+    }
+    else {
+        $_SESSION["Counter"] = 1;
+    }
+    echo("<p>U heeft deze pagina ". $_SESSION["Counter"]. " keer bekeken.</p>");
+?>
+</body>
+</html>
