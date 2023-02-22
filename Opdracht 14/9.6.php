a2ba9faa50f4a15ee2ae2f860e9a26d1b82aa13b
Sofie <sofiebrink@yahoo.com>
2023-02-22 11:48:56 +0100

    Update 9.6.php

--- Opdracht 14/9.6.php
+++ Opdracht 14/9.6.php
@@ -3,15 +3,16 @@
     <input type="text" name="name"><br>
     <label>Bericht</label>
     <textarea rows="4" cols="40" name="text"></textarea><br>
-    <input type="submit" value="Versturen">
+    <input type="submit" name="submit" value="Versturen">
 </form>
 
 
 
 <?php
-    try {
-    if (isset($_POST)) {
-        $MyDB = new PDO("mysql:host=localhost;dbname=Gastenboek", "root", "");
+try {
+    $MyDB = new PDO("mysql:host=localhost;dbname=Gastenboek", "root", "");
+    
+    if (isset($_POST['submit'])) {
         $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
         $text = filter_input(INPUT_POST, "text", FILTER_SANITIZE_STRING);
         $query = $MyDB->prepare("INSERT INTO berichten(naam, bericht) VALUES('$name', '$text')");
@@ -21,10 +22,35 @@
         else {
             echo("Error inserting data into DB");
         }
-        unset($_POST);
     }
+
+    PrintData($MyDB);
+
 } catch(PDOException $e) {
     die("Error!: " . $e->getMessage());
 }
+
+function PrintData($par) {
+    $query = $par->prepare("SELECT * FROM berichten");
+    $query->execute();
+    PrintResult($query->fetchAll(PDO::FETCH_ASSOC));
+}
+
+function PrintResult($par) {
+    //print table
+    if ($par == null) { return null; }
+    echo "<table border=1px>";
+    foreach (array_keys($par[0]) as $key) {
+         echo "<td>" , $key;  "</td>";
+    }
+    foreach ($par as $data) {
+        echo "<tr>";
+        foreach (array_keys($data) as $dat) {
+            echo "<td>" , $data [$dat];  "</td>";
+        }
+        echo "</tr>";
+    }
+    echo '"</table>';
+ }
 
 ?>
