38693617570073b9cd7f39d57d16c48342f79e37
Sofie <sofiebrink@yahoo.com>
2023-03-08 12:11:21 +0100

    Create gastenboek.sql

---
+++ Opdracht 15/gastenboek.sql
@@ -0,0 +1,134 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 08, 2023 at 12:10 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gastenboek`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `berichten`
+--
+
+CREATE TABLE `berichten` (
+  `id` int(9) NOT NULL,
+  `naam` varchar(255) NOT NULL,
+  `bericht` text NOT NULL,
+  `datumtijd` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `berichten`
+--
+
+INSERT INTO `berichten` (`id`, `naam`, `bericht`, `datumtijd`) VALUES
+(43, 'HEnk', 'Erik\r\n', '2023-02-22 10:48:14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `optie`
+--
+
+CREATE TABLE `optie` (
+  `id` int(3) NOT NULL,
+  `pollid` int(3) NOT NULL,
+  `optie` varchar(255) NOT NULL,
+  `stemmen` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `optie`
+--
+
+INSERT INTO `optie` (`id`, `pollid`, `optie`, `stemmen`) VALUES
+(1, 1, 'Inderdaad, PHP is het helemaal', 1),
+(2, 1, 'PHP is leuk, maar niet het leukste', 0),
+(3, 1, 'PHP is saai', 51),
+(4, 2, 'Sofie heeft helemaal gelijk!', 3),
+(5, 2, 'Meneer van Helden heeft het fout', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `poll`
+--
+
+CREATE TABLE `poll` (
+  `id` int(3) NOT NULL,
+  `vraag` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `poll`
+--
+
+INSERT INTO `poll` (`id`, `vraag`) VALUES
+(1, 'Stelling van de maand: \"PHP is de leukste programmeertaal\"'),
+(2, 'Stelling van Sofie: \"PHP is kut, C# is king!\"');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `berichten`
+--
+ALTER TABLE `berichten`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `optie`
+--
+ALTER TABLE `optie`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `poll`
+--
+ALTER TABLE `poll`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `berichten`
+--
+ALTER TABLE `berichten`
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+
+--
+-- AUTO_INCREMENT for table `optie`
+--
+ALTER TABLE `optie`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `poll`
+--
+ALTER TABLE `poll`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
