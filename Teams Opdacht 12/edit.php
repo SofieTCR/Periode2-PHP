e17f914d2f9525d1ba81638d4e7bc41a9f8f07b3
Sofie <sofiebrink@yahoo.com>
2023-03-21 18:27:27 +0100

    Finish CRUD

--- Teams Opdacht 12/edit.php
+++ Teams Opdacht 12/edit.php
@@ -12,21 +12,84 @@
             foreach ($result as $data) {
                 if ($data[GetPrimaryKey($data)] == $_POST["id"]) {
                     // print the form
-                    //var_dump($data);
-                    $form = "<form?>";
+                    $form = "<form method=post>";
                     foreach (array_keys($data) as $dat) {
                         if ($dat != GetPrimaryKey($data)) {
-                            $form .= $dat . "<input type=text value='" . $data[$dat] . "'><br>";
+                            $form .= $dat . "<input type=text name='" . $dat . "' value='" . $data[$dat] . "'><br>";
                         }
                     }
-                    $form .= "<input type=submit value=Wijzigen></form>";
+                    $form .= "<input type=hidden name=sqltable value=" . $_POST["sqltable"] . "><input type=hidden name=id value='" . $_POST["id"] . "'><input type=submit name=submit value=Wijzigen></form>";
                     echo $form;
                 }
             }
         }
         else if ($_POST["submit"] == "Verwijder") {
             // We're deleting the record
-            echo "verwijderen";
+            $result = GetData($_POST["sqltable"]); // grab the table
+            foreach ($result as $data) {
+                if ($data[GetPrimaryKey($data)] == $_POST["id"]) {
+                    $result = $data;
+                }
+            }
+
+            // Connect to the db
+            $MyDB = ConnectDb();
+
+            // Create the SQL string
+            $sql = "DELETE FROM " . $_POST["sqltable"] . " WHERE " . array_keys($result)[0] . " = '" . $result[array_keys($result)[0]] . "'";
+
+            //echo $sql; // echo the SQL statement for testing purposes
+            //exit; // To stop the query from running while we're debugging
+
+            try {
+                $q = $MyDB->Prepare($sql);
+                $q->Execute();
+
+                header("Location: main_brouwers.php");
+            } catch (PDOException $err) {
+                Echo "Error encountered: " . $err;
+            }            
+        }
+        else if ($_POST["submit"] == "Wijzigen") {
+            // Change the actual data
+            $result = GetData($_POST["sqltable"]); // grab the table
+            foreach ($result as $data) {
+                if ($data[GetPrimaryKey($data)] == $_POST["id"]) {
+                    $result = $data;
+                }
+            }
+            $resultkeys = array_keys($result);
+
+            // Connect to the db
+            $MyDB = ConnectDb();
+
+            $sql = "UPDATE " . $_POST["sqltable"] . " SET ";
+            // add all the update statements
+            for ($i=1; $i < count($result); $i++) { 
+                
+                $sql .= $resultkeys[$i] . " = '" . $_POST[$resultkeys[$i]] . "'"; // add the column name plus requested value
+                if ($i != count($result) - 1) {
+                    $sql .= ", "; // add a comma if not the last one
+                }
+                else {
+                    $sql .= " "; // don't add a comma if it is the last one
+                }
+            }
+
+            //add the where clause
+            $sql .= "WHERE " . $resultkeys[0] . " = '" . $result[$resultkeys[0]] . "'"; 
+
+            //echo $sql; // echo the SQL statement for testing purposes
+            //exit; // To stop the query from running while we're debugging
+
+            try {
+                $q = $MyDB->Prepare($sql);
+                $q->Execute();
+
+                header("Location: main_brouwers.php");
+            } catch (PDOException $err) {
+                Echo "Error encountered: " . $err;
+            }
         }
     }
 ?>
